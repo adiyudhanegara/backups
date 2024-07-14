199eb8c6e78621cae69ba22a11185c17f962f4dd
adiyudhanegara <adi.yudhanegaradona@gmail.com>
2024-07-14 16:45:19 +0800

    Implement contact us feature

--- themes/basic/assets/javascript/frontend/main.js
+++ themes/basic/assets/javascript/frontend/main.js
@@ -38,3 +38,66 @@ function backToTop() {
 	document.body.scrollTop = 0;
 	document.documentElement.scrollTop = 0;
 }
+
+$(document).ready( function () {
+	$("#contactForm").validate( {
+		rules: {
+			name: "required",
+			email: {
+				required: true,
+				email: true
+			},
+			phone: {
+				required: true,
+				phone: true
+			},
+			message: {
+				required: true,
+				minlength: 2
+			},
+		},
+		messages: {
+			name: "Please enter your firstname",
+			email: "Please enter a valid email address",
+			phone: "Please enter a valid phone address",
+			message: {
+				required: "Please enter a username",
+				minlength: "Your username must consist of at least 2 characters"
+			},
+		},
+		errorElement: "em",
+		errorPlacement: function ( error, element ) {
+			// Add the `help-block` class to the error element
+			error.addClass( "help-block" );
+
+			// Add `has-feedback` class to the parent div.form-group
+			// in order to add icons to inputs
+			element.parents( ".col-sm-5" ).addClass( "has-feedback" );
+
+			if ( element.prop( "type" ) === "checkbox" ) {
+				error.insertAfter( element.parent( "label" ) );
+			} else {
+				error.insertAfter( element );
+			}
+
+			// Add the span element, if doesn't exists, and apply the icon classes to it.
+			if ( !element.next( "span" )[ 0 ] ) {
+				$( "<span class='glyphicon glyphicon-remove form-control-feedback'></span>" ).insertAfter( element );
+			}
+		},
+		success: function ( label, element ) {
+			// Add the span element, if doesn't exists, and apply the icon classes to it.
+			if ( !$( element ).next( "span" )[ 0 ] ) {
+				$( "<span class='glyphicon glyphicon-ok form-control-feedback'></span>" ).insertAfter( $( element ) );
+			}
+		},
+		highlight: function ( element, errorClass, validClass ) {
+			$( element ).parents( ".col-sm-5" ).addClass( "has-error" ).removeClass( "has-success" );
+			$( element ).next( "span" ).addClass( "glyphicon-remove" ).removeClass( "glyphicon-ok" );
+		},
+		unhighlight: function ( element, errorClass, validClass ) {
+			$( element ).parents( ".col-sm-5" ).addClass( "has-success" ).removeClass( "has-error" );
+			$( element ).next( "span" ).addClass( "glyphicon-ok" ).removeClass( "glyphicon-remove" );
+		}
+	} );
+} );
